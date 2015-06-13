92889d7ef146139de4be295089bd0a97d246a121
Christopher Kramlich <chris@kramlich.com>
2015-06-13 14:18:28 -0700

    Implemented

---
+++ euclids_algo.rb
@@ -0,0 +1,9 @@
+#inspired by http://www.bloomberg.com/graphics/2015-paul-ford-what-is-code/
+#This was basically an opportunity to stretch and work on my math vocabulary.
+def euclid dividend divisor
+
+  remainder = dividend % divisor
+  least_common_divisor = dividend / remainder
+  return least_common_divisor
+
+end
